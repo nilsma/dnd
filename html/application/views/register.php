e8d565aec64e3ae4477d05d9351fcb0fb8f5a273
Nils Martinussen <nilsma231@gmail.com>
2014-05-12 08:26:14 +0200

    fixed registration feedback

--- html/application/views/register.php
+++ html/application/views/register.php
@@ -7,37 +7,6 @@ require_once 'head.php';
 ?>
   <body id="register">
     <h1>Register view</h1>
-
-<?php
-if(isset($_SESSION['reg_failed'])) {
-  echo 'Something went wrong while registering, please try again.';
-  $_SESSION['reg_failed'] = false;
-  unset($SESSION['reg_failed']);
-}
-
-if(isset($_SESSION['fail_user_details_exists'])) {
-  echo 'Username or email already exists!';
-  $_SESSION['fail_user_details_exists'] = false;
-  unset($_SESSION['fail_user_details_exists']);
-}
-
-if(isset($_SESSION['fail_no_password_match'])) {
-  echo 'The password does not match!';
-  $_SESSION['fail_no_password_match'] = false;
-  unset($_SESSION['fail_no_password_match']);
-}
-
-if(isset($_SESSION['fail_password_format'])) {
-  echo 'Wrong format on your passwords!<br/>';
-  echo 'Please make sure that your passwords match,<br/>';
-  echo 'is at least 6 characters long,<br/>';
-  echo 'is not longer that 16 characters,<br/>';
-  echo 'has at least one letter, one number and one uppercase letter!';
-  $_SESSION['fail_password_format'] = false;
-  unset($_SESSION['fail_password_format']);
-}
-
-?>
     <div id="main-container">
       <div class="form-entry">
 	<fieldset>
@@ -54,6 +23,40 @@ if(isset($_SESSION['fail_password_format'])) {
 	    <input type="submit" value="Register">
 	  </form>
 	</fieldset>
+<?php
+if(isset($_SESSION['reg_failed'])) {
+  echo '<div class="reg-error">' . "\n";
+  echo '<p>Something went wrong while registering, please try again.</p>' . "\n";
+  echo '</div>' . "\n";
+  $_SESSION['reg_failed'] = false;
+  unset($SESSION['reg_failed']);
+}
+
+if(isset($_SESSION['fail_user_details_exists'])) {
+  echo '<div class="reg-error">' . "\n";
+  echo '<p>Username or email already exists!</p>' . "\n";
+  $_SESSION['fail_user_details_exists'] = false;
+  unset($_SESSION['fail_user_details_exists']);
+}
+
+if(isset($_SESSION['fail_no_password_match'])) {
+  echo '<div class="reg-error">' . "\n";
+  echo '<p>The passwords does not match!</p>';
+  echo '</div>' . "\n";
+  $_SESSION['fail_no_password_match'] = false;
+  unset($_SESSION['fail_no_password_match']);
+}
+
+if(isset($_SESSION['fail_password_format'])) {
+  echo '<div class="reg-error">' . "\n";
+  echo '<p>Wrong format on your passwords!</p>' . "\n";
+  echo '<p>Please make sure that your passwords match is at least 6 characters long, is not longer that 16 characters, has at least one letter, one number and one uppercase letter!</p> . "\n"';
+  echo '</div>' . "\n";
+  $_SESSION['fail_password_format'] = false;
+  unset($_SESSION['fail_password_format']);
+}
+
+?>
       </div> <!-- end .form-entry -->
       <section class="sec-nav-container">
 	<p class="nav-paragraph">Or <a href="index.php">Go Back To The Home Page</a></p>
